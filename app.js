a47313787aca9b2f2b34488aaa5cfcda67d02372
Jthuillier <67591400+Jthuillier@users.noreply.github.com>
2020-10-29 13:04:58 +0100

    Init

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+    function tired() {
+        document.getElementById("login-sidebar").style.display = "block";
+    }
+    function kill() {
+        document.getElementById("login-sidebar").style.display = "none";
+    }
+
+// Je devrais pas me répeter comme un sale comme ça mais je mettrais beaucoups trop de temps a faire autrement
+    function ouvrir() {
+        document.getElementById("inscription-sidebar").style.display = "block";
+    }
+    function exit() {
+        document.getElementById("inscription-sidebar").style.display = "none"
+    }
